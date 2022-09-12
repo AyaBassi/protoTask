e3884520052506d73a9918d12ef0d52546867928
aya bassi <pkarbassi@hotmail.co.uk>
2022-09-12 14:02:11 +0100

    1st Commit: 1.Set sceneDelegate and appDelegate to program view components programatically 2. Add anchor and textfield extensions

--- ProtoTask/AppDelegate.swift
+++ ProtoTask/AppDelegate.swift
@@ -11,9 +11,19 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    var window: UIWindow?
+    let viewController = ViewController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if #available(iOS 13.0, *){
+            //do nothing we will have code in SceneDelegate for this
+        } else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = viewController
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
